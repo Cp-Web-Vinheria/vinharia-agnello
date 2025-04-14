ff76ced2f3b6d42707393d60b95ae1ae2cb375a4
E-brinca64 <enzo.brincalepe@gmail.com>
2025-04-14 17:48:03 -0300

    Cadastro de idade

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,20 @@ if (nome == ""){
     alert("Falha, digite algo para continuar");
     var nome = prompt("digite seu nome:");
 }
+// idade 
+var anoNascimento = prompt("digite seu ano de nascimento: ")
+const idade = 2025 - anoNascimento
+// Caso da idade vazio 
+if(anoNascimento = ""){
+    alert("Falha, digite algo para continuar");
+    var anoNascimento = prompt("digite seu ano de nascimento: ")
+}
+// Caso menor de idade 
+else if (idade < 18){
+    alert(`Você é menor de idade volte em ${18 - idade} anos`)
+}
+
+else{
+    alert("Cadastro realizado! Veja os detalhes no console.")
+
+}
