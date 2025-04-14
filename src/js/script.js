c45e817fae67046f4ba5760c4fb7af8ab63478d3
E-brinca64 <enzo.brincalepe@gmail.com>
2025-04-14 17:47:09 -0300

    Cadastro de nome

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// Nome 
+var nome = prompt("digite seu nome:");
+// Caso do nome vazio 
+if (nome == ""){
+    alert("Falha, digite algo para continuar");
+    var nome = prompt("digite seu nome:");
+}
